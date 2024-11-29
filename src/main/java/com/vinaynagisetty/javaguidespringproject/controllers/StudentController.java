9f8aa3d626432cf3cc755e7e782bb57fb8ab0bf6
vinay nagisetty <83533201+vinaynagisetty@users.noreply.github.com>
2024-11-29 06:46:07 +0000

    craeted the get api of array of students

--- src/main/java/com/vinaynagisetty/javaguidespringproject/controllers/StudentController.java
+++ src/main/java/com/vinaynagisetty/javaguidespringproject/controllers/StudentController.java
@@ -1,5 +1,8 @@
 package com.vinaynagisetty.javaguidespringproject.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,19 @@ public class StudentController {
     public Student StudentDetails(){
         return new Student("vinay", "Nagisetty");
     }
+
+
+    @GetMapping ("/students")
+    public List<Student> StudentsDetails(){
+      
+        List<Student> students=new ArrayList<>();
+        students.add(new Student("vinay","nagisetty"));
+        students.add(new Student("vamsi","nagisetty"));
+        students.add(new Student("vinay","fernadeze"));
+        students.add(new Student("JAck","Tyson"));
+        students.add(new Student("Thoams","Edison"));
+            return students;
+        
+     }
 
 }
