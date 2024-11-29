1b55cb05a251e57147e6ee1146400ea766a24360
vinay nagisetty <83533201+vinaynagisetty@users.noreply.github.com>
2024-11-29 07:06:55 +0000

    pathvarible with stattic data end point

--- src/main/java/com/vinaynagisetty/javaguidespringproject/controllers/StudentController.java
+++ src/main/java/com/vinaynagisetty/javaguidespringproject/controllers/StudentController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vinaynagisetty.javaguidespringproject.Entity.Student;
@@ -29,5 +30,19 @@ public class StudentController {
             return students;
         
      }
+
+     @GetMapping ("/{firstName}/{lastName}")
+     //If we chanaged  pathvarible name in the method arguments we have  mention in @pathvarible("what name we gave in getmapping url")
+    public Student studentPathVaribleEXample(@PathVariable String firstName ,@PathVariable("lastName") String lastname1 ){
+        return new Student(firstName, lastname1);
+    }
+
+    @GetMapping ("/student/{firstName}/{lastName}")
+    //If we chanaged  pathvarible name in the method arguments we have  mention in @pathvarible("what name we gave in getmapping url")
+   public Student studentPathVaribleEXamplewithsaticstudentname(@PathVariable String firstName ,@PathVariable("lastName") String lastname1 ){
+       return new Student(firstName, lastname1);
+   }
+
+
 
 }
